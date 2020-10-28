bb95b0da49d6c657e64e9520c705d1712d359f79
Pierre Verkest <pierreverkest84@gmail.com>
2020-10-28 15:11:33 +0100

    Setup using cookie cutter

---
+++ anyblok_background_tasks/core_task/__init__.py
@@ -0,0 +1,31 @@
+"""Blok declaration example
+"""
+from anyblok.blok import Blok
+
+
+class Core_task(Blok):
+    """Core_task's Blok class definition
+    """
+    version = "0.1.0"
+    author = "Pierre Verkest"
+    required = ['anyblok-core']
+
+    @classmethod
+    def import_declaration_module(cls):
+        """Python module to import in the given order at start-up
+        """
+        from . import model  # noqa
+
+    @classmethod
+    def reload_declaration_module(cls, reload):
+        """Python module to import while reloading server (ie when
+        adding Blok at runtime
+        """
+        from . import model  # noqa
+        reload(model)
+
+    def update(self, latest):
+        """Update blok"""
+        # if we install this blok in the database we add a new record
+        if not latest:
+            self.registry.Example.insert(name="An example")
---
+++ doc/source/apidoc.rst
@@ -0,0 +1,7 @@
+API Documentation
+=================
+
+.. automodule:: anyblok_background_tasks.core_task
+   :members:
+   :undoc-members:
+   :show-inheritance:
